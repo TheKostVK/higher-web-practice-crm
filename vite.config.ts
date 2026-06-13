a13e80239f2d7156fc28090a5a956e5cfb2b3784
TheKost_ <kostvkthekost@gmail.com>
2026-06-13 16:00:51 +0300

    feat(ci): добавить автоматический деплой фронта на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const projectRoot = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/higher-web-practice-crm/',
     plugins: [react(), svgr()],
     resolve: {
         alias: {
